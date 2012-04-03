51e549208f3d8a1c28779bdb9798c9bb9263bf20
Piotr Giedziun <piotrgiedziun@gmail.com>
2012-04-03 18:26:26 +0300

    short usage information

--- projekt_1/Release/run.sh
+++ projekt_1/Release/run.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+# usage:
+# ./run.sh [input file path] [storage_type] [algorithm]
+#
+# storage_type = matrix/list
+# algorithm    = dijkstra/ford
+#
+# example:
+# ./run.sh ../input/default.wiki.jjd matrix dijkstra
+
 rm output.txt
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
